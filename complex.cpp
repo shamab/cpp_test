fe163f4f0a4d676d8b5cef7a56fbd9eae4da40a3
shamab <baldishama@gmail.com>
2015-05-19 10:57:09 +0200

    Added addition functionality
    could not edit the file from terminal

--- complex.cpp
+++ complex.cpp
@@ -7,7 +7,7 @@ int real,imaginary;
 };
 int main()
 {
-	complex a,b;
+	complex a,b,c;
 	a.real;
 	cout<<"Enter real part of first number in a+ib"<<endl;
 	cin>>a.real;
@@ -18,5 +18,15 @@ int main()
 	cin>>b.real;
 	cout<<"Enter imaginary part"<<endl;
 	cin>>b.imaginary;
+	c.real=a.real+b.real;
+	c.imaginary=a.imaginary+b.imaginary;
+	if(c.imaginary>0)
+	{	
+		cout<<"Resultant of addition is"<<c.real<<"+"<<"i"<<c.imaginary<<endl;
+	}
+	else
+	{
+		cout<<"Resultant of addition is"<<c.real<<"-"<<"i"<<c.imaginary<<endl;
+	}
 
 }
